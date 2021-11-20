dbc8f2cfbd1b7acc1660003f042300bb4f9bb86d
namkma309 <73546952+namkma309@users.noreply.github.com>
2021-11-20 22:48:42 +0700

    Update config.js

--- config.js
+++ config.js
@@ -8,6 +8,6 @@ const contentCard = 'Chúc Em yêuuu của Anh sinh nhật tràn ngập niềm v
 // Cần hỗ trợ hãy liên hệ: 
 // Mr-Nam http://facebook.com/nam.nodemy
 // Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
-const giftImage = 'hot-girl.png';
+const giftImage = 'hot-girl.jpg';
 const base64 = '';
-const giftImageBase64 = "data:image/png;base64, " + base64;
+const giftImageBase64 = "data:image/jpg;base64, " + base64;
